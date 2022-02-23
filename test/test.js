1c3ca5c23105bead5d21168ba3ba076d5b14fe9d
Donald Pakkies <donaldpakkies@gmail.com>
2022-02-23 21:05:01 +0000

    feat: add new test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
 const { Command } = require('./setup/command')
 
 describe('Test CLI', () => {
-    it('returns help', () => {
+    it('displays cli help', () => {
         const output = Command.call('', ['--no-ansi'])
         
         expect(output).toContain("Console 1.0")
+    })
+
+    it('returns hello world', () => {
+        expect(Command.call('hello', ['world', '--no-ansi'])).toContain('Hello world')
     })
 })
