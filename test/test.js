12229e252a4678712c55ec4ccab3f280cf25cebe
Donald Pakkies <donaldpakkies@gmail.com>
2022-02-23 21:10:00 +0000

    chore: add more rules

--- test/test.js
+++ test/test.js
@@ -5,9 +5,11 @@ describe('Test CLI', () => {
         const output = Command.call('', ['--no-ansi'])
         
         expect(output).toContain("Console 1.0")
+        expect(output).toContain("Usage:")
+        expect(output).toContain("Available commands:")
     })
 
     it('returns hello world', () => {
-        expect(Command.call('hello', ['world', '--no-ansi'])).toContain('Hello world')
+        expect(Command.call('hello', ['world', '--no-ansi'])).toBe('Hello world')
     })
 })
