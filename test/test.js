c86a4f85df7e202811777844e1d07bee142cc01a
Donald Pakkies <donaldpakkies@gmail.com>
2022-07-22 19:34:16 +0200

    feat: add tests for events

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const { Command } = require('./setup/command')
 describe('Test CLI', () => {
     it('displays cli help', () => {
         const output = Command.call('', ['--no-ansi'])
-        
+
         expect(output).toContain("Console 1.0")
         expect(output).toContain("Usage:")
         expect(output).toContain("Available commands:")
@@ -11,5 +11,17 @@ describe('Test CLI', () => {
 
     it('returns hello world', () => {
         expect(Command.call('hello', ['world', '--no-ansi'])).toBe('Hello world')
+    })
+
+    it('runs event', () => {
+        const output = Command.call('', ['--no-ansi'])
+
+        expect(output).toContain("681c7325-300a-4867-95b2-1e7f1846413a")
+    })
+
+    it('doesn\'t run event', () => {
+        const output = Command.call('hello', ['world', '--no-ansi'])
+
+        expect(output).not.toContain("681c7325-300a-4867-95b2-1e7f1846413a")
     })
 })
